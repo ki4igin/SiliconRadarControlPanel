24a1a760f0879be4c7b5684ae1d352f721812ed6
artem <ki4igin1992@outlook.com>
2022-03-10 00:38:53 +0300

    Добавил логирование

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,13 +5,36 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Serilog;
 
-namespace SiliconRadarControlPanel
+namespace SiliconRadarControlPanel;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+
+    protected override void OnStartup(StartupEventArgs e)
     {
+        base.OnStartup(e);
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("logs/myapp.txt")
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Information("Start sesion");
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+
+        Log.Information("Close sesion");
+        Log.CloseAndFlush();
     }
 }
+
+
