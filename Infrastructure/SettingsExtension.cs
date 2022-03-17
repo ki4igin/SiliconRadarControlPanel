63bc6d8b216410958156fbb38d5e3dcb6cebc717
artem <ki4igin1992@outlook.com>
2022-03-17 17:05:29 +0300

    Добавил стартовые настройки для DDS

---
+++ Infrastructure/SettingsExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace SiliconRadarControlPanel.Infrastructure;
+
+public interface ISettings
+{
+
+}
+
+public static class SettingsExtension
+{
+    public static void Save<T>(this T settings) where T : ISettings
+    {
+        var dirPath = AppContext.BaseDirectory + "/settings";
+        var fileName = $".{settings.GetType().Name.ToLower()}";
+        var filePath = $"{dirPath}/{fileName}.json";
+        Directory.CreateDirectory(dirPath);
+        string jsonString = settings.SaveToStr();
+        File.WriteAllText(filePath, jsonString);
+    }
+    public static void Read<T>(this T settings) where T : ISettings
+    {
+        var dirPath = AppContext.BaseDirectory + "/settings";
+        var fileName = $".{settings.GetType().Name.ToLower()}";
+        var filePath = $"{dirPath}/{fileName}.json";
+        if (File.Exists(filePath) is false)
+            return;
+        var jsonString = File.ReadAllText(filePath);
+        settings.ReadFromStr(jsonString);
+    }
+
+    private static string SaveToStr<T>(this T settings) where T : ISettings
+    {
+        var jsonOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+        return JsonSerializer.Serialize(settings, jsonOptions);
+    }
+    private static void ReadFromStr<T>(this T settings, string str) where T : ISettings
+    {
+        var jsonOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+        var newSettings = JsonSerializer.Deserialize<T>(str, jsonOptions);
+        CopyAllProperty(newSettings, settings);
+    }
+    private static void CopyAllProperty<T>(T source, T target)
+    {
+        var type = typeof(T);
+        foreach (var sourceProperty in type.GetProperties())
+        {
+            var targetProperty = type.GetProperty(sourceProperty.Name);
+            targetProperty.SetValue(target, sourceProperty.GetValue(source, null), null);
+        }
+    }
+}
--- ViewModels/DDSViewModels/DDSViewModel.cs
+++ ViewModels/DDSViewModels/DDSViewModel.cs
@@ -1,13 +1,13 @@
 ﻿using SiliconRadarControlPanel.Commands.Base;
+using SiliconRadarControlPanel.Infrastructure;
 using SiliconRadarControlPanel.Services;
 using SiliconRadarControlPanel.ViewModels.Base;
+using System;
 
 namespace SiliconRadarControlPanel.ViewModels.DDSViewModels;
 
-public class DDSViewModel : TitledViewModel
+public class DDSViewModel : TitledViewModel, IDisposable
 {
-    private readonly Communication _communication;
-
     public DDSR0ViewModel DDSR0ViewModel { get; init; }
     public DDSR1ViewModel DDSR1ViewModel { get; init; }
     public DDSR2ViewModel DDSR2ViewModel { get; init; }
@@ -17,18 +17,36 @@ public class DDSViewModel : TitledViewModel
     public DDSR6ViewModel DDSR6ViewModel { get; init; }
     public DDSR7ViewModel DDSR7ViewModel { get; init; }
 
-    public DDSViewModel() : this(new Communication()) { }
+    //public DDSViewModel() : this(new Communication()) { }
 
     public DDSViewModel(Communication communication)
     {
-        DDSR0ViewModel = new DDSR0ViewModel(communication);
-        DDSR1ViewModel = new DDSR1ViewModel(communication);
-        DDSR2ViewModel = new DDSR2ViewModel(communication);
-        DDSR3ViewModel = new DDSR3ViewModel(communication);
-        DDSR4ViewModel = new DDSR4ViewModel(communication);
-        DDSR5ViewModel = new DDSR5ViewModel(communication);
-        DDSR6ViewModel = new DDSR6ViewModel(communication);
-        DDSR7ViewModel = new DDSR7ViewModel(communication);
-        _communication = communication;
+        DDSModel ddsModel = new(0, 0, 0, 0, 0, 0, 0, 0);
+        ddsModel.Read();
+        DDSR0ViewModel = new DDSR0ViewModel(communication) { RegisterValue = ddsModel.R0 };
+        DDSR1ViewModel = new DDSR1ViewModel(communication) { RegisterValue = ddsModel.R1 };
+        DDSR2ViewModel = new DDSR2ViewModel(communication) { RegisterValue = ddsModel.R2 };
+        DDSR3ViewModel = new DDSR3ViewModel(communication) { RegisterValue = ddsModel.R3 };
+        DDSR4ViewModel = new DDSR4ViewModel(communication) { RegisterValue = ddsModel.R4 };
+        DDSR5ViewModel = new DDSR5ViewModel(communication) { RegisterValue = ddsModel.R5 };
+        DDSR6ViewModel = new DDSR6ViewModel(communication) { RegisterValue = ddsModel.R6 };
+        DDSR7ViewModel = new DDSR7ViewModel(communication) { RegisterValue = ddsModel.R7 };
+    }
+
+    public record DDSModel(uint R0, uint R1, uint R2, uint R3, uint R4, uint R5, uint R6, uint R7) : ISettings;
+
+    public void Dispose()
+    {
+        DDSModel ddsModel = new(
+            DDSR0ViewModel.RegisterValue,
+            DDSR1ViewModel.RegisterValue,
+            DDSR2ViewModel.RegisterValue,
+            DDSR3ViewModel.RegisterValue,
+            DDSR4ViewModel.RegisterValue,
+            DDSR5ViewModel.RegisterValue,
+            DDSR6ViewModel.RegisterValue,
+            DDSR7ViewModel.RegisterValue
+        );
+        ddsModel.Save();
     }
 }
